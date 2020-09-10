1b1a839e181b0424a5e961008e7076973eed20bb
Josh Cook <joshuacook0023@gmail.com>
2020-09-10 07:54:55 -0400

    feat: create view for main Workout session
    (barebones)

---
+++ Workout Spinner WatchKit Extension/Views/WorkoutView.swift
@@ -0,0 +1,37 @@
+//
+//  WorkoutView.swift
+//  Workout Spinner WatchKit Extension
+//
+//  Created by Joshua on 9/10/20.
+//  Copyright © 2020 Joshua Cook. All rights reserved.
+//
+
+import SwiftUI
+
+struct WorkoutView: View {
+    
+    var workout: Workout
+    
+    var body: some View {
+        Text(workout.displayName)
+    }
+}
+
+struct WorkoutView_Previews: PreviewProvider {
+    static var workouts: Workouts {
+        var ws = Workouts()
+        let i = ws.workouts.first { $0.type == .count }!
+        let j = ws.workouts.first { $0.type == .time }!
+        ws.workouts = [i, j]
+        return ws
+    }
+    
+    static var previews: some View {
+        Group {
+            ForEach(workouts.workouts) { workout in
+                WorkoutView(workout: workout)
+                    .previewDisplayName(workout.displayName)
+            }
+        }
+    }
+}
