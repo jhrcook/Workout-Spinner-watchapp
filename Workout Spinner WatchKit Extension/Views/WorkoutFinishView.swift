832618469e5d6c9e8c3983ea2b8693c213f17335
Josh Cook <joshuacook0023@gmail.com>
2020-10-30 07:53:29 -0400

    feat: show list of summary stats

--- Workout Spinner WatchKit Extension/Views/WorkoutFinishView.swift
+++ Workout Spinner WatchKit Extension/Views/WorkoutFinishView.swift
@@ -8,6 +8,37 @@
 
 import SwiftUI
 
+
+struct InfoRowView: View {
+    let title: String
+    let titleColor: Color
+    let value: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title).foregroundColor(titleColor).font(.system(size: 13))
+                Spacer()
+            }
+            HStack {
+                Text(value).font(.system(size: 15))
+                Spacer()
+            }
+        }
+    }
+}
+
+struct DoneButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(5)
+            .background(
+                RoundedRectangle(cornerRadius: 25, style: .continuous).fill(Color(red: 194, green: 255, blue: 60))
+            )
+    }
+}
+
+
 struct WorkoutFinishView: View {
     
     @ObservedObject var workoutManager: WorkoutManager
@@ -15,15 +46,40 @@ struct WorkoutFinishView: View {
     
     @Environment(\.presentationMode) var presentationMode
     
+    private var averageHR: String {
+        return valueAsIntStringOrNA(workoutTracker.averageHeartRate)
+    }
+    private var minHR: String {
+        return valueAsIntStringOrNA(workoutTracker.minHeartRate)
+    }
+    private var maxHR: String {
+        return valueAsIntStringOrNA(workoutTracker.maxHeartRate)
+    }
+    
     var body: some View {
         VStack {
-            Text("\(workoutTracker.numberOfExercises) exercises")
-            Button(action: {
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                Text("Done")
+            List {
+                InfoRowView(title: "Number of exercises", titleColor: .green, value: "\(workoutTracker.numberOfExercises)")
+                InfoRowView(title: "Active Calories", titleColor: .yellow, value: "\(Int(workoutTracker.totalActiveCalories))")
+                InfoRowView(title: "Average heart rate", titleColor: .red, value: averageHR)
+                InfoRowView(title: "Min/Max heart rate", titleColor: .red, value: "\(minHR) / \(maxHR)")
+                
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Done")
+                        .foregroundColor(.black)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                }
+                .listStyle(PlainListStyle())
+                .listRowPlatterColor(.clear)
+                .buttonStyle(DoneButtonStyle())
             }
         }
+    }
+    
+    func valueAsIntStringOrNA(_ x: Double?) -> String {
+        return x == nil ? "NA" : "\(Int(x!.rounded()))"
     }
 }
 
