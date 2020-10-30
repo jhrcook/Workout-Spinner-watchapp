487f2daf861b6e58b7ac91bec3777906641412f4
Josh Cook <joshuacook0023@gmail.com>
2020-10-30 07:53:19 -0400

    feat: WorkoutTracker calculates summary stats
    - total time, total Cal, avg. HR, min/max HR

--- Workout Spinner WatchKit Extension/Models/WorkoutTracker.swift
+++ Workout Spinner WatchKit Extension/Models/WorkoutTracker.swift
@@ -15,6 +15,35 @@ class WorkoutTracker: NSObject, ObservableObject {
         data.count
     }
     
+    var totalActiveCalories: Double {
+        return data.map { $0.activeCalories }.reduce(0, +)
+    }
+    
+    var totalDuration: Double {
+        return data.map { $0.duration }.reduce(0, +)
+    }
+    
+    var averageHeartRate: Double? {
+        if data.count < 1 { return nil }
+        let HRtotal = data.map { $0.heartRate.reduce(0, +) }.reduce(0, +)
+        let HRcount = data.map { $0.heartRate.count}.reduce(0, +)
+        if HRcount == 0 { return nil }
+        return HRtotal / Double(HRcount)
+    }
+    
+    var maxHeartRate: Double? {
+        if data.count < 1 { return nil }
+        let x = data.map { ($0.heartRate.max()) ?? 0 }.max() ?? 0
+        return x == 0.0 ? nil : x
+    }
+    
+    var minHeartRate: Double? {
+        if data.count < 1 { return nil }
+        let x = data.map { ($0.heartRate.min()) ?? 0 }.min() ?? 0
+        return x == 0.0 ? nil : x
+    }
+    
+    
     func addData(info: ExerciseInfo, duration: Double, activeCalories: Double, heartRate: [Double]) {
         let newData = WorkoutTrackerDatum(exerciseInfo: info, duration: duration, activeCalories: activeCalories, heartRate: heartRate)
         data.append(newData)
