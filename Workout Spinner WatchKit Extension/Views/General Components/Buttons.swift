cc5b99ac3bff30b4e4b55fd42507408a5da20670
Josh Cook <joshuacook0023@gmail.com>
2020-11-11 08:39:25 -0500

    refactor:buttons and button styles
    - specifically a button that works in a List (plain style)

---
+++ Workout Spinner WatchKit Extension/Views/General Components/Buttons.swift
@@ -0,0 +1,31 @@
+//
+//  Buttons.swift
+//  Workout Spinner WatchKit Extension
+//
+//  Created by Joshua on 11/11/20.
+//  Copyright © 2020 Joshua Cook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListViewTextButton: View {
+    
+    var label: String
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(label)
+                .foregroundColor(.black)
+                .frame(minWidth: 0, maxWidth: .infinity)
+        }
+        .listStyle(PlainListStyle())
+        .listRowPlatterColor(.clear)
+    }
+}
+
+struct Buttons_Previews: PreviewProvider {
+    static var previews: some View {
+        ListViewTextButton(label: "Button") {}
+    }
+}
