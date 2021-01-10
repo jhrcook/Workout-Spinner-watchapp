71148cacfe232ee7e15034fd842440f9562fe169
Josh Cook <joshuacook0023@gmail.com>
2021-01-10 08:41:48 -0500

    Revert "rename files to remove spaces"
    This reverts commit 08b18e536fd4c6d25b0e6a4d1d5dc4817389d503.
    
    I screwed something up during this and XCode couldn't run the app
    in the simulator.

--- Workout Spinner WatchKit Extension/View Models/WorkoutPickerViewModel.swift
+++ Workout Spinner WatchKit Extension/View Models/WorkoutPickerViewModel.swift
@@ -31,12 +31,7 @@ extension ExercisePicker {
             pointingSlice = Double(numExercises) + pointingSlice
         }
 
-        logger.debug("reading spinner selection: pointing angle: \(Int(pointingAtAngle.rounded()))")
-        logger.debug("reading spinner selection - pointing slice: \(Int(pointingSlice))")
-
-//        workoutManager.exerciseInfo = exerciseOptions.exercises[selectedExerciseIndex]
-        selectedExerciseIndex = min(Int(pointingSlice), numExercises - 1)
         workoutManager.exerciseInfo = exerciseOptions.exercises[selectedExerciseIndex]
-        logger.debug("reading spinner selection - selected index: \(selectedExerciseIndex)")
+        selectedExerciseIndex = min(Int(pointingSlice), numExercises - 1)
     }
 }
